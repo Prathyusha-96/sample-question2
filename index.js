d2403c185aef72eb578ec7104a65348f9b0d8132
prathyusha <>
2021-10-16 11:18:37 +0530

    UPDATE JS

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ var btnHeading2 = document.querySelector("#btn-heading2");
 var btnHeading3 = document.querySelector("#btn-heading3");
 var output = document.querySelector("#output"); 
 btnHeading1.addEventListener("click", h1)
-function h1(){
-    userInput.value === (h1);
-     output.innerText = userInput.value;
+function h1() {
+    userInput.style.fontSize = "25px";
+}
+btnHeading2.addEventListener("click", h2)
+function h2() {
+    userInput.style.fontSize = "15px";
+}
+btnHeading3.addEventListener("click", h3)
+function h3() {
+    userInput.style.fontSize = "10px";
 }
 
